a4db8b1ecc8e07b921237aa5c18f210068ace3b8
thaisemanuele <thatitis@gmail.com>
2019-09-26 11:54:45 +0300

    fixing stock prices

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -10,7 +10,7 @@ const mutations = {
     }  ,
     'RDN_STOCKS' (state) {
         state.stocks.forEach(stock => {
-           stock.price = Math.round(stock.price * (1 + Math.random() - 0.5));
+           stock.price = Math.ceil(stock.price * (1 + Math.random() - 0.5));
         });
     }
 };
